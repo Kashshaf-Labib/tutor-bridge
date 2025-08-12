bc844cece23e4ca317a6d222025e764d5b9032b9
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-08-12 21:02:03 +0600

    feat(Context): add authContext for user, login, register and logout functionality

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = async (email, password) => {
+    const res = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: { "content-Type": "apllication/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await res.json();
+    if (res.ok) {
+      setUser(data);
+    } else {
+      throw new Error(data.message);
+    }
+  };
+
+  const register = async (name, email, password, role, phone) => {
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password, role, phone }),
+    });
+    const data = await res.json();
+    if (res.ok) setUser(data);
+    else throw new Error(data.message);
+  };
+
+  const logout = () => setUser(null);
+
+  return (
+    <AuthContext.Provider value={{ user, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
