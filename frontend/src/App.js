74351aa99e97b1593605346e32d034651d7827d8
whiteflags26 <whiteflags26@gmail.com>
2025-08-28 22:47:16 +0600

    feat(App): add routes for Profile and PostDetails components based on user authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import Login from "../src/components/Auth/Login";
 import Register from "../src/components/Auth/Register";
 import Home from "../src/pages/Home";
 import About from "../src/pages/About";
+import Profile from "../src/pages/Profile";
+import PostDetails from "../src/pages/PostDetails";
 // import Explore from "../src/pages/Explore";
 import Navbar from "../src/components/Layout/Navbar";
 import "./App.css";
@@ -24,6 +26,12 @@ function AppContent() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         {/* <Route path="/explore" element={<Explore />} /> */}
+        {user && (
+          <>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/post-details/:id" element={<PostDetails />} />
+          </>
+        )}
         {!user && (
           <>
             <Route path="/login" element={<Login />} />
