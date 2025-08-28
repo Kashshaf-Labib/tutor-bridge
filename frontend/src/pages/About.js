938bef96fc287836160d889ec7cc9c83e00de7c0
yasirraiyan <yasirraiyan@iut-dhaka.edu>
2025-08-28 15:14:40 +0600

    create about page and add Our Mission title

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./About.module.css";
+
+const aboutData = [
+  {
+    title: "Our Mission",
+    description:
+      "TutorBridge connects students and tutors in a seamless, secure, and friendly environment. Our mission is to empower learners and educators with the tools they need to succeed.",
+    icon: "🎯",
+  },
+  
+  
+];
+
+function About() {
+  return (
+    <div className={styles.aboutPage}>
+      
+    </div>
+  );
+}
+
+export default About;
